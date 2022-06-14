60dd6d24ed55612a47b376659bfe925d857aa8ca
Alex de Wergifosse <alex@edgeworx.io>
2022-06-14 16:24:31 +1200

    Add debug for copy button

--- assets/js/clipboard.js
+++ assets/js/clipboard.js
@@ -8,30 +8,26 @@ for (var i = 0; i < pre.length; ++ i)
   var mermaid = element.getElementsByClassName('language-mermaid')[0];
 
   if (mermaid == null) {
-    element.insertAdjacentHTML('afterbegin', '<button class="btn btn-copy"></button>');
+    element.insertAdjacentHTML('afterbegin', `<button class="btn btn-copy"></button>`);
   }
 }
 
 var clipboard = new Clipboard('.btn-copy', {
-
-  target: function(trigger) {
-    return trigger.nextElementSibling;
-  },
-
-});
+    target: (trigger) => {
+      return trigger.parentElement.getElementsByTagName('code')[0]
+    }
+})
 
 clipboard.on('success', function(e) {
-
-    /*
     console.info('Action:', e.action);
     console.info('Text:', e.text);
     console.info('Trigger:', e.trigger);
-    */
 
     e.clearSelection();
 });
 
 clipboard.on('error', function(e) {
+    console.error({ e })
     console.error('Action:', e.action);
     console.error('Trigger:', e.trigger);
 });
